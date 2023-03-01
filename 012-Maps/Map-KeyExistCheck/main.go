67e99998f8aea794d4de92545f900582f6e5558b
Dsp <92397962+durgadaspatil04@users.noreply.github.com>
2023-03-01 10:25:26 +0530

    changed keyExist logic missing part

--- 012-Maps/Map-KeyExistCheck/main.go
+++ 012-Maps/Map-KeyExistCheck/main.go
@@ -12,7 +12,8 @@ func main() {
 	// ok is Boolean returns true-Present flase-Not Present value Returns Value OF the key
 	if value, ok := Emp[check]; ok{
 		fmt.Println("The Salary of Mike", value)
-	}
+	}else{
 	fmt.Println("The Key NOt Found!!")
+	}
 	fmt.Printf("Type ok - %T Value %v\n", ok, value)
 }
