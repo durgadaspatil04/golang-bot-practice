749c8c160d704102e1c2d71bcef2196ffea62d4c
Dsp <92397962+durgadaspatil04@users.noreply.github.com>
2024-09-07 11:33:00 +0530

    Update main.go

--- 004-Types/Type-Int/main.go
+++ 004-Types/Type-Int/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	// as uint8 range is 255 so u cannot store value above 255 error Overflow occurs
-	var sizeCm uint8 = 255
+	var sizeCm uint8 = 255 // cannot use 256 (untyped int constant) as uint8 value in variable declaration (overflows)
 	var a int
 	fmt.Println("Value is", sizeCm)
 	fmt.Printf("Type is %T\n", sizeCm)                           //prints data type
